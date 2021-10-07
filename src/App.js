87810afa45b51d1a3d000e86f1d8056d1d8fcba5
gedwards97 <georgeedwards97@hotmail.com>
2021-10-07 14:01:40 +0100

    Policy App Setup
    Initially uploading a very simple web app which just displays an instance of a policy. This demonstrates that the policy component is able to successfully fetch policy data from the API, with the app being built upon further to involve a user authentication process.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import UserPolicy from './components/UserPolicy';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Policy App!</h1>
       </header>
+      <UserPolicy />
     </div>
   );
 }
---
+++ src/components/UserPolicy.js
@@ -0,0 +1,60 @@
+import { useState, useEffect } from 'react'
+
+const UserPolicy = () => {
+
+    // Component States
+    const [loading, setLoading] = useState(false);
+    const [policy, setPolicy] = useState({});
+    const [vehicle, setVehicle] = useState({});
+    const [address, setAddress] = useState({});
+
+    const url = "https://api.bybits.co.uk/policys/details";
+    const options = {
+        headers: {
+            'environment': "mock",
+            'Authorization': "Bearer MuYW1hem9uYXdzLmNvbVwvZXUtd2VzdC0zX3JkdldSMGs",
+            'Content-type': "application/json"
+        }
+    };
+
+    const fetchPolicy = async () => {
+        try {
+            setLoading(true);
+            const response = await fetch(url, options);
+            const json_data = await response.json();
+            setPolicy(json_data.policy);
+            setVehicle(json_data.vehicle);
+            setAddress(json_data.policy.address);
+
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        fetchPolicy();
+    }, []);
+
+    return (
+        <div>
+            {!loading ? 
+            <div className="policy-container">
+                <h2>My Policy</h2>
+                <h3>Policy Reference:</h3>
+                <p>{policy.policy_reference}</p>            
+                <h3>Cover Type:</h3>
+                <p>{policy.cover}</p>
+                <h3>Car:</h3>
+                <p>{vehicle.make + " " + vehicle.model + " " +
+                vehicle.colour + "-" + vehicle.reg}</p>
+                <h3>Address:</h3>
+                <p>{address.line_1 + ", " + address.line_2 + ", " + address.postcode}</p>
+            </div> : <h2>LOADING</h2>
+            }
+        </div> 
+    )
+}
+
+export default UserPolicy;
